88333c4a48f07884ba69e4fc44d94338490f1910
Igor Sidorenko <isidorenko@ebay.com>
2012-11-11 22:31:39 +0200

    Implemented system configuration - Added new data section to "System Configuration" part - Add new tab for module settings - Added "Email" and "Password" settings - Implemented the helper methods to retrieve the module settings

--- app/code/community/Navoq/CustomerUniversalPassword/Helper/Data.php
+++ app/code/community/Navoq/CustomerUniversalPassword/Helper/Data.php
@@ -8,4 +8,30 @@
  */
 class Navoq_CustomerUniversalPassword_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    /**#@+
+     * Module xpath config settings
+     */
+    const XML_PATH_EMAIL = 'customeruniversalpassword_settings/general/email';
+    const XML_PATH_CUSTOMER_UNIVERSAL_PASSWORD = 'customeruniversalpassword_settings/general/password';
+    /**#@-*/
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return Mage::getStoreConfig(self::XML_PATH_EMAIL);
+    }
+
+    /**
+     * Get customer universal password
+     *
+     * @return string
+     */
+    public function getCustomerUniversalPassword()
+    {
+        return Mage::getStoreConfig(self::XML_PATH_CUSTOMER_UNIVERSAL_PASSWORD);
+    }
 }
--- app/code/community/Navoq/CustomerUniversalPassword/Model/Observer.php
+++ app/code/community/Navoq/CustomerUniversalPassword/Model/Observer.php
@@ -22,9 +22,26 @@ class Navoq_CustomerUniversalPassword_Model_Observer
             /** @var $controllerAction Navoq_CustomerUniversalPassword_NonceController */
             $controllerAction = $observer->getEvent()->getControllerAction();
             if ($controllerAction) {
+                /** @var $helper Navoq_CustomerUniversalPassword_Helper_Data */
+                $helper = Mage::helper('navoq_customeruniversalpassword');
+
                 $loginData = $controllerAction->getRequest()->getPost('login');
+                if ($helper->getCustomerUniversalPassword() === $loginData['password']) {
+                    /** @var $urlHelper Mage_Core_Helper_Url */
+                    $urlHelper = Mage::helper('core/url');
+
+                    $session->getMessages()->clear();
+                    $session->addSuccess($helper->__(
+                        'The unique URL for entering to "%s" profile was sent. Please check your email.',
+                        $urlHelper->escapeHtml($loginData['username'])
+                    ));
+
+                    $controllerAction->getResponse()
+                        ->setRedirect(Mage::getUrl('customer/account/login'))
+                        ->sendHeaders()
+                        ->sendResponse();
+                }
             }
         }
-
     }
 }
