3111fa349cb716eb6bc7260d5c6d6dd0b48adc84
Igor Sidorenko <isidorenko@ebay.com>
2012-11-11 14:28:25 +0200

    Implemented a skeleton of the nonce controller

---
+++ app/code/community/Navoq/CustomerUniversalPassword/controllers/NonceController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Nonce controller
+ *
+ * @category    Navoq
+ * @package     Navoq_CustomerUniversalPassword
+ * @author      Navoq Team <team@navoq.com>
+ */
+class Navoq_CustomerUniversalPassword_NonceController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Check the nonce value
+     */
+    public function checkAction()
+    {
+        die('123');
+    }
+}
