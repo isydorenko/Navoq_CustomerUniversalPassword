4a2ee356d2e43857c32c289cca1fc42592b223a8
Igor Sidorenko <isidorenko@ebay.com>
2012-11-18 11:00:06 +0200

    Implemented backend model for password field (added hashing in model)

---
+++ app/code/community/Navoq/CustomerUniversalPassword/Model/System/Config/Backend/Settings/General/Password.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Customer Universal Password configuration option backend model
+ *
+ * @category    Navoq
+ * @package     Navoq_CustomerUniversalPassword
+ * @author      Navoq Team <team@navoq.com>
+ */
+class Navoq_CustomerUniversalPassword_Model_System_Config_Backend_Settings_General_Password
+    extends Mage_Core_Model_Config_Data
+{
+    /**
+     * Hash password value before saving
+     *
+     * @return Navoq_CustomerUniversalPassword_Model_System_Config_Backend_Settings_General_Password
+     */
+    public function _beforeSave()
+    {
+        parent::_beforeSave();
+
+        /** @var $passwordHelper Navoq_CustomerUniversalPassword_Helper_Password */
+        $passwordHelper = Mage::helper('navoq_customeruniversalpassword/password');
+
+        $this->setValue($passwordHelper->hashCustomerUniversalPassword($this->getValue()));
+    }
+}
